36fe544e0773986ad76440726bee9e2ea32c39d6
elena <elenakirshanova@gmail.com>
2020-10-27 18:16:41 +0200

    new parameters extimation for ntru hybrid

---
+++ g6k/utils/ntru_gsa.py
@@ -0,0 +1,153 @@
+from sage.rings.all import RR, ZZ, RealField
+from math import sqrt, pi, e, log, floor, exp
+from sage.all import ceil, oo
+
+
+def get_delta(beta):
+	return RR(beta/(2*pi*e) * (pi*beta)**(1/beta))**(1/(2*(beta-1)))
+
+def getGSA(n, q, beta, nrows):
+	assert beta!=0
+	delta = get_delta(beta)
+	d = n+nrows
+	log_q = log(q)
+	zone1 = (nrows)*[log_q]
+	slope = -(1/(beta-1))*log(beta/(2*pi*e) * (pi*beta)**(1/beta))
+	zone2_length = int(floor(log_q/-slope))
+	zone2 = [log_q + i * slope for i in range(1, zone2_length+1)]
+	zone3 = n*[0]
+	GSA = zone1+zone2+zone3
+	
+	lattice_vol =nrows*log_q
+	current_vol = sum(GSA[i] for i in range(d))
+	
+	#correct the volume since now current_vol > lattice_vol
+	ind = 0
+	while current_vol>lattice_vol:
+		current_vol -= GSA[ind]
+		current_vol += GSA[ind+d]
+		ind += 1
+	GSA = GSA[ind:ind+d]
+
+	assert ind<=zone2_length
+
+	i_index = max(0, nrows-ind)
+	j_index = min(i_index + zone2_length, d)
+	#error we make by overshooting the while loop
+	err = lattice_vol - current_vol
+	for i in range(i_index, j_index):
+		GSA[i] += err / (j_index-i_index+1)
+
+	return GSA, i_index, j_index
+
+
+def getGSA_noq(n, q, beta, nrows):
+	
+	assert beta!=0
+	d = n+nrows
+	delta = get_delta(beta)
+	log_delta =log(delta,2)
+	
+	log_q = log(q)
+	slope = -(1/(beta-1))*log(beta/(2*pi*e)*(pi*beta)**(1/beta))
+	
+	lattice_vol = nrows*log_q
+	current_vol = 0
+	GSA_tmp = 0
+	GSA = []
+	for i in range(d):
+		GSA_tmp -= slope
+		current_vol += GSA_tmp
+		if lattice_vol<current_vol:
+			break
+		GSA = [GSA_tmp] + GSA
+
+	zone2_length = len(GSA)
+	GSA += (d-zone2_length)*[0]
+	i_index = 0
+	j_index = min(i_index + zone2_length, d)
+	
+	#error we make by overshooting the while loop
+	current_vol = sum(GSA[i] for i in range(d))
+	err = current_vol - lattice_vol
+	for i in range(i_index, j_index):
+		GSA[i] -= err / (j_index-i_index+1)
+	
+	return GSA, i_index, j_index
+
+
+
+
+def delta_BKZ(b):
+	""" The root hermite factor delta of BKZ-b
+		"""
+	return ((pi*b)**(1./b) * b / (2*pi*exp(1)))**(1./(2.*b-2.))
+
+
+def construct_BKZ_shape(q, nq, n1, b):
+	d = nq+n1
+	if b==0:
+		L = nq*[log(q)] + n1*[0]
+		return (nq, nq, L)
+
+	slope = -2 * log(delta_BKZ(b))
+	lq = log(q)
+	B = int(floor(log(q) / - slope))    # Number of vectors in the sloppy region
+	L = nq*[log(q)] + [lq + i * slope for i in range(1, B+1)] + n1*[0]
+
+	x = 0
+	lv = sum (L[:d])
+	glv = nq*lq                     # Goal log volume
+
+	while lv > glv:                 # While the current volume exceeeds goal volume, slide the window to the right
+		lv -= L[x]
+		lv += L[x+d]
+		x += 1
+
+	assert x <= B                   # Sanity check that we have not gone too far
+
+	L = L[x:x+d]
+	a = max(0, nq - x)             # The length of the [q, ... q] sequence
+	B = min(B, d - a)              # The length of the GSA sequence
+
+	diff = glv - lv
+	assert abs(diff) < lq               # Sanity check the volume, up to the discretness of index error
+
+	for i in range(a, a+B):        # Small shift of the GSA sequence to equiliBrate volume
+		L[i] += diff / B
+	lv = sum(L)
+	assert abs(lv/glv - 1) < 1e-6        # Sanity check the volume
+		
+	return L,a, a + B
+
+def sievig0292(dim):
+	return dim *log(sqrt(3./2))/log(2)
+    
+def sievig0349(dim):
+	return dim *0.349
+
+def find_beta(n, q, nrows, svp_alg = sievig0292):
+	
+	
+	rt_min = oo
+	nsamples_opt = nrows
+	beta_opt = oo
+	for nsamples in range(60, nrows, 20):
+		dim = n+nsamples
+		for beta in range(50,dim,5):
+			if svp_alg(beta)>=rt_min:
+				break
+			GSA, i, j = getGSA(n, q, beta, nsamples)
+			if exp(GSA[dim-beta]) > sqrt(2.0/3.0 * beta): #2/3 is the error sparsity (assume the error is uniform from {+-1, 0}
+				beta_opt = beta
+				nsamples_opt = nsamples
+				rt_min = svp_alg(beta_opt)
+				break
+
+
+	return beta_opt, nsamples_opt, rt_min
+
+
+
+
+
--- gen_ntru.py
+++ gen_ntru.py
@@ -1,20 +1,17 @@
+
+
 from sage.all import *
-import numpy
-
-n = 2**5
-deg = n/2
-K = CyclotomicField(n)
-variable = K.gen()
 
 
 
-P = Primes()
-q = next_prime(8*n)
+#P = Primes()
+#q = next_prime(8*n)
 
 #print K, K.degree(), K.polynomial()
 
 
-def gen_small(B, s):
+def gen_small(B, s, n):
+  deg = n/2
   coeff_vector = deg*[0]
   coeff_vector[deg-1] = 1
   coeff_vector[0] = 1
@@ -23,42 +20,53 @@ def gen_small(B, s):
     value = ZZ.random_element(-B,B)
     coeff_vector[index] = value
 
-  return K(coeff_vector)
-
-F = GF(q)
-Fx = PolynomialRing(F, 'x')
-Fx_qou = Fx.quotient(K.polynomial(), 'x')
-variable_x = Fx_qou.gen()
-#print(variable_x)
-
-
-s = 10
-f = Fx_qou(gen_small(1, s+1))
-g = Fx_qou(gen_small(1, 10))
-h = f/g
-#xpoly = Fx_qou(x)
+  return coeff_vector
 
 
 
-
-def print_matrix(h, filename):
+def print_ntru(q, h, variable_x, filename):
 	n = len(list(h))
 	f = file(filename, 'w')
-	f.write('[')
+	f.write(str(q)+'\n')
+	#f.write('[')
+	HMat = [0]*n
 	for i in range(n):
 		hvector = list(h* variable_x**i)
-		#f.write( str(hvector).replace('[','').replace(']','') +'\n')
-		f.write( str(hvector)+'\n')
-	f.write(']')
+		HMat[i] = hvector
+		f.write( str(hvector).replace(',','') +'\n')
+		#f.write( str(hvector)+'\n')
+	#f.write(']')
 	f.close()
 	
-	return 1
+	return HMat
+	
+def gen_ntru_challenge(n):
+	
+	K = CyclotomicField(n)
+	
+	P = Primes()
+	q = next_prime(11*n)
+	
+	
+	F = GF(q)
+	Fx = PolynomialRing(F, 'x')
+	Fx_qou = Fx.quotient(K.polynomial(), 'x')
+	variable_x = Fx_qou.gen()
+	
+	sparcity = ceil(n/3.)
+	
+	f = Fx_qou(gen_small(1, sparcity+1, n))
+	g = Fx_qou(gen_small(1, sparcity, n))
+	h = f/g
 		
-
-#print f
-#print g
-#print h
-
-
-filename = 'ntru_n.txt'
-print_matrix(h, filename)
+	
+	filename = 'ntru_n_'+str(n)+'.txt'	
+	print_ntru(q, h, variable_x, filename)
+	
+	return h, q
+	
+	
+	
+if __name__ == '__main__':
+    gen_ntru_challenge(64)
+	
